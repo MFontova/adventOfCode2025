3116e9d9205f43901f4359c643f6872a4a55031e
mfontova <mfontova@synergie.es>
2025-12-12 17:54:12 +0100

    14 midudev

---
+++ src/14/midudev.ts
@@ -0,0 +1,71 @@
+type Gift = string | number | boolean
+type Workshop = Record<string, any>
+type Path = string[]
+
+function findGiftPath(workshop: Workshop, gift: Gift): Path {
+
+  let result: Path = []
+
+  Object.keys(workshop).forEach(k => {
+    console.log('k', k)
+    if(typeof k === 'string' && workshop[k].toString() === gift.toString()) {
+      result = [k]
+    } else if(typeof workshop[k] !== 'string') {
+      Object.keys(workshop[k]).forEach(subK => {
+        console.log('subK', subK)
+        if(typeof subK === 'string' && workshop[k][subK].toString() === gift.toString()) {
+          result = [k, subK]
+        } else if(typeof workshop[k][subK] !== 'string') {
+          Object.keys(workshop[k][subK]).forEach(subSubK => {
+            console.log('subSubK', subSubK)
+            if(typeof subSubK === 'string' && workshop[k][subK][subSubK].toString() == gift.toString()) {
+              console.log('found')
+              result = [k, subK, subSubK]
+            }
+          })
+        }
+      })
+    }
+  })
+
+  console.log(result)
+  return result
+}
+
+const workshop = {
+  ok: true,
+  nested: {
+    nope: false,
+    extra: {
+      is: 0
+    }
+  }
+}
+
+findGiftPath(workshop, false)
+
+// const workshop = {
+//   storage: {
+//     shelf: {
+//       box1: 'train',
+//       box2: 'switch'
+//     },
+//     box: 'car'
+//   },
+//   gift: 'doll'
+// }
+
+// findGiftPath(workshop, 'train')
+// // ➜ ['storage', 'shelf', 'box1']
+
+// findGiftPath(workshop, 'switch')
+// // ➜ ['storage', 'shelf', 'box2']
+
+// findGiftPath(workshop, 'car')
+// // ➜ ['storage', 'box']
+
+// findGiftPath(workshop, 'doll')
+// // ➜ ['gift']
+
+// findGiftPath(workshop, 'plane')
+// // ➜ []
